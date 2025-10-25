7bbd607a6a081ec1915e5730600e975408a85ffa
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-10-25 23:35:59 +0200

    Task completed

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -1,8 +1,27 @@
 package mate.academy
-
-// implement the `getMiddleString()` function, which takes a string
-// and returns middle character (or characters) as a string
+private const val TAX_RATE = 2
+private const val NUMBER_ZERO = 0
+private const val NUMBER_MAGIG = 1
+// реалізувати функцію `getMiddleString()`, яка приймає рядок
+// та повертає середній символ (або символи) як рядок
 
 fun getMiddleString(originalString: String): String {
-    return ""
+    val length = originalString.length
+    if (length == NUMBER_ZERO) {
+        return ""
+    }
+    val middleIndex = length / TAX_RATE
+    if (length % TAX_RATE == NUMBER_ZERO) {
+        return originalString.substring(middleIndex- NUMBER_MAGIG, middleIndex + NUMBER_MAGIG)
+    } else {
+        return originalString.substring(middleIndex , middleIndex + NUMBER_MAGIG)
+
+    }
+}
+fun main() {
+    println("hello -> \"${getMiddleString("hello")}\"")
+    println("1234 -> \"${getMiddleString("1234")}\"")
+    println("(()) -> \"${getMiddleString("(())")}\"")
+    println("Bob -> \"${getMiddleString("Bob")}\"")
+    println(" -> \"${getMiddleString("")}\"")
 }
