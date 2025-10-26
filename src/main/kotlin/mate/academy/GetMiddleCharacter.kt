eb3f3724ea060292048ae50c7adc21f35e8d12a7
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-10-26 16:01:44 +0100

    Task completed2

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -5,19 +5,19 @@ private const val NUMBER_MAGIG = 1
 // реалізувати функцію `getMiddleString()`, яка приймає рядок
 // та повертає середній символ (або символи) як рядок
 
-fun getMiddleString(originalString: String): String {
-    val length = originalString.length
-    if (length == NUMBER_ZERO) {
-        return ""
-    }
-    val middleIndex = length / TAX_RATE
-    if (length % TAX_RATE == NUMBER_ZERO) {
-        return originalString.substring(middleIndex- NUMBER_MAGIG, middleIndex + NUMBER_MAGIG)
+fun getMiddleString(originalString: String): String =
+    if (originalString.isEmpty()) {
+         ""
     } else {
-        return originalString.substring(middleIndex , middleIndex + NUMBER_MAGIG)
-
+        val length = originalString.length
+        val middleIndex = length / TAX_RATE
+        if (length % TAX_RATE == NUMBER_ZERO) {
+            originalString.substring(middleIndex - NUMBER_MAGIG, middleIndex + NUMBER_MAGIG)
+        } else {
+            originalString.substring(middleIndex, middleIndex + NUMBER_MAGIG)
+        }
     }
-}
+
 fun main() {
     println("hello -> \"${getMiddleString("hello")}\"")
     println("1234 -> \"${getMiddleString("1234")}\"")
